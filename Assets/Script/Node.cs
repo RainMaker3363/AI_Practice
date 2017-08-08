7617302a12665f2a59f8021545dfba15f0f274e3
RainMaker3363 <jsc22637@gmail.com>
2017-08-09 04:41:21 +0900

    #Sync 1
    - AI 알고리즘 연습
    - A* 기반 짜는 중..

---
+++ Assets/Script/Node.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node {
+    public bool walkabls;
+    public Vector3 worldPosition;
+
+    public Node(bool _walkable, Vector3 _worldPos)
+    {
+        walkabls = _walkable;
+        worldPosition = _worldPos;
+    }
+}
